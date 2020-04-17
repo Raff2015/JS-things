c182f5c35fb8525f14336460bc8ea96a8a5b7f23
Edgar Chicas <erc7@njit.edu>
2020-04-16 22:02:35 -0400

    updates

--- yearly_rent.js
+++ yearly_rent.js
@@ -76,4 +76,18 @@ for (let i = 0; i <= 10; i++) {
 }
 console.log(f);
 
+function addTwo(number) {
+  return number + 2;
+}
+
+//Functions and Scope
+const finalAnswer = addTwo(5);
+console.log(finalAnswer);
+
+function greet(firstName, lastName, honorific, greeting) {
+  return `${greeting} ${honorific} ${lastName}! I’m extremely pleased you could join us, ${firstName}! I hope you enjoy your stay, ${honorific} ${lastName}.`;
+}
+
+console.log(greet("Brian", "Holt", "Lord", "Salutations"));
+console.log(greet("Jack", "Sparrow", "Captain", "A-hoy"));
 
