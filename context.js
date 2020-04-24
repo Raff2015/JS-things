d97c11e10a58ad2ff1e7fb120cb89baba9efb605
Edgar Chicas <erc7@njit.edu>
2020-04-23 21:10:55 -0400

    Context

---
+++ context.js
@@ -0,0 +1,23 @@
+const me = {
+    name: {
+      first: "Jeremiah",
+      middle: "Rafael",
+      last: "Chicas"
+    },
+    location: {
+      streetNumber: 500,
+      street: "Fakestreet",
+      city: "Seattle",
+      state: "WA",
+      zipCode: 55555,
+      country: "USA"
+    },
+    getAddress() {
+      return `${this.name.first} ${this.name.middle} ${this.name.last}
+  ${this.location.streetNumber} ${this.location.street}
+  ${this.location.city}, ${this.location.state} ${this.location.zipCode}
+  ${this.location.country}`;
+    }
+  };
+  
+  console.log(me.getAddress());
