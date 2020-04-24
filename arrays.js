3ca55f7a7ebeef5024d2e0337d838aad65ad8883
Edgar Chicas <erc7@njit.edu>
2020-04-23 22:09:47 -0400

    updatz

--- arrays.js
+++ arrays.js
@@ -16,3 +16,19 @@ const daysOfTheWeek = [
   const primeNumbers = [1, 2, 3, 5, 7, 11, 13, 17];
 console.log(primeNumbers.length);
 console.log(primeNumbers.join(" | "));
+
+
+const courses = [
+    { teacher: "Kyle Simpson", course: "JS Function Lite" },
+    { teacher: "Sarah Drasner", course: "Intro to Vue" },
+    { teacher: "Brian Holt", course: "Complete Intro to React v3" },
+    { teacher: "Steve Kinney", course: "State Management" }
+  ];
+  
+  courses.push({ teacher: "Sean Larkinn", course: "Webpack" });
+  
+  console.log(courses);
+  
+  courses[2] = { teacher: "Brian Holt", course: "Complete Intro to React v4" };//This overwrites course from v3 to v4
+  
+  console.log(courses);
