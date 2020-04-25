ed1a8fc41e99b52c1b1431f298ddd945876b7360
Edgar Chicas <erc7@njit.edu>
2020-04-24 22:18:30 -0400

    more updatz

--- push.js
+++ push.js
@@ -11,7 +11,15 @@ for (let i = 0; i < cities.length; i++) {
     console.log(cities[i]);
 }
 
-// method 2
-cities.forEach(function(city) { //This method is more easier
+// method 2 ~ This method is more easier
+cities.forEach(function(city) {
     console.log(city);
 });
+
+// Same as method 3 but with more stuff
+let numberOfTimesRan = 0;
+cities.forEach(function(city, index) {
+    numberOfTimesRan++;
+    console.log(index, city.toLowerCase());
+});
+console.log(numberOfTimesRan);
